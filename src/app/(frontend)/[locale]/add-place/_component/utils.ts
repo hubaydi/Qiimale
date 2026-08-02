e89cd2bccdf3795eea9928ff0c889894e68050ed
Mohamed Hobaydi <cajaaib252@gmail.com>
2026-08-02 07:12:28 +0200

    Moved some folders to improve organization

--- src/app/(frontend)/[locale]/add-place/_component/utils.ts
+++
@@ -1,8 +0,0 @@
-export function getLocalizedName(
-  name: string | { so?: string; en?: string },
-  locale: string,
-): string {
-  if (typeof name === "string") return name;
-  if (!name) return "";
-  return (locale === "en" ? name.en : name.so) || name.so || name.en || "";
-}
